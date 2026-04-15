f880b60d891d9ceaa947c4840b17936e532172c3
RaishAnwar <140946680+RaishAnwar@users.noreply.github.com>
2026-04-16 00:34:53 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+--Create DATABASE WAREHOUSE
+
+
+use master;
+
+
+CREATE DATABASE DataWarehouse;
+
+use DataWarehouse;
+
+---Here schemas are created  
+create schema bronze;
+Go
+create schema silver;
+go
+create schema gold;
+go
